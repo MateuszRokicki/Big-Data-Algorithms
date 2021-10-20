f9a28d1c2fc4c5046f8fbf9d62ed9d8ccc45dcf3
Mateusz Rokicki <mateuszjrokicki@gmail.com>
2021-10-20 15:23:42 +0200

    Exercise2

---
+++ src/main/scala/Ex1.scala
@@ -0,0 +1,6 @@
+object Ex1 {
+  def main(args: Array[String]) : Unit = {
+    println("Hello, world!")
+  }
+
+}
---
+++ src/main/scala/Ex2.scala
@@ -0,0 +1,31 @@
+import scala.io.Source
+import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.ListMap
+import scala.collection.mutable.Map
+object Ex2 {
+  def main(args: Array[String]) : Unit = {
+    val stopwords = List("ourselves", "hers", "between", "yourself", "but", "again", "there", "about", "once", "during", "out", "very", "having", "with", "they", "own", "an", "be", "some", "for", "do", "its", "yours", "such", "into", "of", "most", "itself", "other", "off", "is", "s", "am", "or", "who", "as", "from", "him", "each", "the", "themselves", "until", "below", "are", "we", "these", "your", "his", "through", "don", "nor", "me", "were", "her", "more", "himself", "this", "down", "should", "our", "their", "while", "above", "both", "up", "to", "ours", "had", "she", "all", "no", "when", "at", "any", "before", "them", "same", "and", "been", "have", "in", "will", "on", "does", "yourselves", "then", "that", "because", "what", "over", "why", "so", "can", "did", "not", "now", "under", "he", "you", "herself", "has", "just", "where", "too", "only", "myself", "which", "those", "i", "after", "few", "whom", "t", "being", "if", "theirs", "my", "against", "a", "by", "doing", "it", "how", "further", "was", "here", "than")
+    var lines = Source.fromFile("D:/Studia/Magisterskie/II semestr/Big Data Algorithms/Laboratorium/Lab0_1/orwell1984.txt").getLines().mkString(" ")
+    var words_f = lines.split(" ")
+    var words = ListBuffer[String]()
+    for (word <- words_f) {
+      var word_L = word.toLowerCase().replaceAll("\\p{Punct}", "")
+      if (!stopwords.contains(word_L)) {
+        words += word_L
+      }
+    }
+    val word_count: Map[String, Int] = Map()
+    for (word <- words) {
+      if (word_count.contains(word)) {
+        word_count(word) += 1
+      }
+      else {
+        word_count += (word -> 1)
+      }
+    }
+    val res = word_count.toSeq.sortWith(_._2 > _._2)
+    for(r <- res.take(40)){
+      println(r._2 + " " + r._1)
+    }
+  }
+}
