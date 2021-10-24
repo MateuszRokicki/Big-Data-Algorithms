6fcf0b84d9cf93d4c5eb99c3863b6dc084e5c606
Mateusz Rokicki <mateuszjrokicki@gmail.com>
2021-10-24 11:19:26 +0200

    Exercise3_update

--- src/main/scala/Lab0/Ex2.scala
+++ src/main/scala/Lab0/Ex2.scala
@@ -6,7 +6,7 @@ import scala.io.Source
 object Ex2 {
   def main(args: Array[String]): Unit = {
     val stopwords = List("ourselves", "hers", "between", "yourself", "but", "again", "there", "about", "once", "during", "out", "very", "having", "with", "they", "own", "an", "be", "some", "for", "do", "its", "yours", "such", "into", "of", "most", "itself", "other", "off", "is", "s", "am", "or", "who", "as", "from", "him", "each", "the", "themselves", "until", "below", "are", "we", "these", "your", "his", "through", "don", "nor", "me", "were", "her", "more", "himself", "this", "down", "should", "our", "their", "while", "above", "both", "up", "to", "ours", "had", "she", "all", "no", "when", "at", "any", "before", "them", "same", "and", "been", "have", "in", "will", "on", "does", "yourselves", "then", "that", "because", "what", "over", "why", "so", "can", "did", "not", "now", "under", "he", "you", "herself", "has", "just", "where", "too", "only", "myself", "which", "those", "i", "after", "few", "whom", "t", "being", "if", "theirs", "my", "against", "a", "by", "doing", "it", "how", "further", "was", "here", "than")
-    var lines = Source.fromFile("D:/Studia/Magisterskie/II semestr/Big Data Algorithms/Laboratorium/Lab0_1/orwell1984.txt").getLines().mkString(" ")
+    var lines = Source.fromFile("D:\\Studia\\Magisterskie\\II semestr\\Big Data Algorithms\\Laboratorium\\Lab0_1\\files\\Lab_0\\orwell1984.txt").getLines().mkString(" ")
     var words_f = lines.split(" ")
     var words = ListBuffer[String]()
     for (word <- words_f) {
@@ -25,8 +25,10 @@ object Ex2 {
       }
     }
     val res = word_count.toSeq.sortWith(_._2 > _._2)
-    for (r <- res.take(40)) {
-      println(r._2 + " " + r._1)
+    for (r <- res.take(41)) {
+      if(r._1!=""){
+        println(r._2 + ", " + r._1)
+      }
     }
   }
 }
--- src/main/scala/Lab0/Ex3.scala
+++ src/main/scala/Lab0/Ex3.scala
@@ -2,19 +2,16 @@ package Lab0
 
 import scala.collection.mutable.{ListBuffer, Map}
 import scala.io.Source
-import java.io.{BufferedWriter, File, FileWriter, PrintWriter}
-//import au.com.bytecode.opencsv.CSVWriter
-//import com.opencsv.
+import java.io.{File, FileNotFoundException, PrintWriter}
 
 
 
 class Text() {
-  //mapa z (word,count)
+
   private var word_count: Map[String, Int] = Map()
   var sorted_words = Map[String,Int]().toSeq
   val stopwords = List("ourselves", "hers", "between", "yourself", "but", "again", "there", "about", "once", "during", "out", "very", "having", "with", "they", "own", "an", "be", "some", "for", "do", "its", "yours", "such", "into", "of", "most", "itself", "other", "off", "is", "s", "am", "or", "who", "as", "from", "him", "each", "the", "themselves", "until", "below", "are", "we", "these", "your", "his", "through", "don", "nor", "me", "were", "her", "more", "himself", "this", "down", "should", "our", "their", "while", "above", "both", "up", "to", "ours", "had", "she", "all", "no", "when", "at", "any", "before", "them", "same", "and", "been", "have", "in", "will", "on", "does", "yourselves", "then", "that", "because", "what", "over", "why", "so", "can", "did", "not", "now", "under", "he", "you", "herself", "has", "just", "where", "too", "only", "myself", "which", "those", "i", "after", "few", "whom", "t", "being", "if", "theirs", "my", "against", "a", "by", "doing", "it", "how", "further", "was", "here", "than")
 
-  //funkcja mapująca Stringa -> sprawdź czy istnieje takie słowo -> +1, nie ma stwórz nowy
   def CountWords(input_string:String): Unit = {
     var lines = Source.fromFile(input_string).getLines().mkString(" ")
     var words_f = lines.split(" ")
@@ -35,29 +32,24 @@ class Text() {
     }
   }
 
-  //print do konsoli
   def Word_Cloud(): Unit = {
     sorted_words = word_count.toSeq.sortWith(_._2 > _._2)
     for (s <- sorted_words.take(40)) {
       if(s._1!=""){
         println(s._2 + " " + s._1)
       }
-      //println(r._2 + " " + r._1)
     }
   }
-    //zapis do pliku
+
   def Save(path:String, title: String): Unit = {
     val file_path = path + '\\' + title + ".csv"
     val pw = new PrintWriter(new File(file_path))
-    for(s<-sorted_words.take(40)){
+    for(s<-sorted_words.take(41)){
       if(s._1!=""){
         pw.write(s._2 + ", " + s._1 + '\n')
       }
-      //pw.write(s._2+','+s._1+'\n')
     }
     pw.close()
-    //val outputFile = new BufferedWriter(new FileWriter(file_path))
-    //val csvWriter = new CSVWriter(outputFile)
   }
 }
 
@@ -80,9 +72,8 @@ object Ex3 {
         }
       }
       catch{
-        case e: NumberFormatException => println()
+        case e: FileNotFoundException => println("Wrong path")
       }
     }
   }
-
 }
